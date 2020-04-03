ff61d2210b3afe1942c5a3f8458559d338c79960
WeisonWei <weixx3@126.com>
2020-04-03 13:29:30 +0800

    init

---
+++ submodule-add.sh
@@ -0,0 +1,23 @@
+#!/usr/bin bash
+git submodule add https://github.com/WeisonWei/e-school-service-student.git
+git submodule add https://github.com/WeisonWei/e-school-service-user.git
+git submodule add https://github.com/WeisonWei/e-school-service-score.git
+git submodule add https://github.com/WeisonWei/e-school-service-feign.git
+git submodule add https://github.com/WeisonWei/e-school-service-ribbon.git
+git submodule add https://github.com/WeisonWei/e-school-service-zuul.git
+git submodule add https://github.com/WeisonWei/e-school-service-es.git
+git submodule add https://github.com/WeisonWei/e-school-service-product.git
+git submodule add https://github.com/WeisonWei/e-school-service-sales.git
+git submodule add https://github.com/WeisonWei/e-school-service-order.git
+git submodule add https://github.com/WeisonWei/e-school-service-account.git
+git submodule add https://github.com/WeisonWei/e-school-service-api.git
+git submodule add https://github.com/WeisonWei/e-school-service-base.git
+git submodule add https://github.com/WeisonWei/e-school-service-task.git
+git submodule add https://github.com/WeisonWei/e-school-service-gateway.git
+git submodule add https://github.com/WeisonWei/e-school-service-feign.git
+git submodule add https://github.com/WeisonWei/e-school-service-discovery.git
+git submodule add https://github.com/WeisonWei/e-school-service-config.git
+git submodule add https://github.com/WeisonWei/e-school-service-admin.git
+#submodule add 执行完后执行
+#git submodule init
+#git submodule update
